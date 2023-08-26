1545240cb724889b3029928297f815f56460ca79
johntech-GIT <tea-expansion@yandex.ru>
2023-08-26 22:14:43 +0300

    10

--- NewsPaper/NewsPortal/signals.py
+++ NewsPaper/NewsPortal/signals.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save, m2m_changed
 from django.dispatch import receiver
 from django.core.mail import mail_managers
@@ -6,6 +7,12 @@ from .tasks import new_post_subscription
 
 
 @receiver(m2m_changed, sender=PostCategory,)
+def notify_subscribers(sender, instаnce, **kwargs):
+    if kwargs['action'] == 'post_add':
+        pass
+        new_post_subscription(instаnce)
+
+@receiver(post_save, sender=User,)
 def notify_subscribers(sender, instаnce, **kwargs):
     if kwargs['action'] == 'post_add':
         pass
